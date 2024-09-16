cbb8cb3db70fa2d7fccd10d434d81e140f7c5d07
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-09-16 15:25:28 +0500

    Added informational message for better UX

--- Assets/javascript/homePage.js
+++ Assets/javascript/homePage.js
@@ -6,3 +6,14 @@ let imgDisplay = document.querySelector("#form_img");
 imgInput.onchange = () => {
   imgDisplay.src = URL.createObjectURL(imgInput.files[0]);
 };
+
+// Image information msg
+
+let imgUploadBtn = document.querySelector(".choose_img_label");
+let imgInfoMsg = document.querySelector("#img_info");
+imgUploadBtn.addEventListener("mouseenter", () => {
+  imgInfoMsg.style.opacity = 1;
+});
+imgUploadBtn.addEventListener("mouseleave", () => {
+  imgInfoMsg.style.opacity = 0;
+});
