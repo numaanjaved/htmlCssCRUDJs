1bb9b24d6ec772c3c4251e6449010c4c7daea78c
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-09-17 12:48:27 +0500

    Data object created and test performed

--- Assets/javascript/homePage.js
+++ Assets/javascript/homePage.js
@@ -42,36 +42,37 @@ form.addEventListener("submit", (e) => {
   if (userFirstName.value == "" || userFirstName == null) {
     FnameErr.innerHTML = `Please fill the First Name field`;
     FnameErr.style.display = "block";
+    userFirstName.value = "";
   } else {
     if (!userFirstName.value.match(/^[a-zA-Z]{1,}$/)) {
       FnameErr.innerHTML = `Please match the given example`;
-      userFirstName.value = "";
       FnameErr.style.display = "block";
+      userFirstName.value = "";
     } else {
       FnameErr.innerHTML = "";
       FnameErr.style.display = "none";
-      userFirstName.value = "";
     }
   }
   // Last Name Validation
   if (userLastName.value == "" || userLastName == null) {
     LnameErr.innerHTML = `Please fill the Last Name field`;
     LnameErr.style.display = "block";
+    userLastName.value = "";
   } else {
     if (!userLastName.value.match(/^[a-zA-Z]{1,}$/)) {
       LnameErr.innerHTML = `Please match the given example`;
-      userLastName.value = "";
       LnameErr.style.display = "block";
+      userLastName.value = "";
     } else {
       LnameErr.innerHTML = "";
       LnameErr.style.display = "none";
-      userLastName.value = "";
     }
   }
   //   Email Validation
   if (userEmail.value == "" || userEmail == null) {
     emailErr.innerHTML = `Please fill the Email field`;
     emailErr.style.display = "block";
+    userEmail.value = "";
   } else {
     if (
       !userEmail.value.match(
@@ -79,56 +80,72 @@ form.addEventListener("submit", (e) => {
       )
     ) {
       emailErr.innerHTML = `Please match the given example`;
-      userEmail.value = "";
       emailErr.style.display = "block";
+      userEmail.value = "";
     } else {
       emailErr.innerHTML = "";
       emailErr.style.display = "none";
-      userEmail.value = "";
     }
   }
   //   Contact Number Validation
   if (userContactNumber.value == "" || userContactNumber == null) {
     contactErr.innerHTML = `Please fill the Contact Number field`;
     contactErr.style.display = "block";
+    userContactNumber.value = "";
   } else {
     if (!userContactNumber.value.match(/^\+[0-9]{2,}[0-9]{7,}$/)) {
       contactErr.innerHTML = `Please match the given example`;
-      userContactNumber.value = "";
       contactErr.style.display = "block";
+      userContactNumber.value = "";
     } else {
       contactErr.innerHTML = "";
       contactErr.style.display = "none";
-      userContactNumber.value = "";
     }
   }
   //   Address Validation
   if (userAddress.value == "" || userAddress == null) {
     addressErr.innerHTML = `Please fill the Address field`;
     addressErr.style.display = "block";
+    userAddress.value = "";
   } else {
     if (!userAddress.value.match(/^[a-zA-Z0-9\s,.'-]{3,}$/)) {
       addressErr.innerHTML = `Please match the given example`;
-      userAddress.value = "";
       addressErr.style.display = "block";
+      userAddress.value = "";
     } else {
       addressErr.innerHTML = "";
       addressErr.style.display = "none";
-      userAddress.value = "";
     }
   }
   //   Bio Validation
   if (userBio.value == "" || userBio == null) {
     bioErr.innerHTML = `Please fill the Profile description field`;
     bioErr.style.display = "block";
+    userBio.value = "";
   }
+
+  let usersDataArray = [
+    {
+      FirstName: userFirstName.value,
+      LastName: userLastName.value,
+      Email: userEmail.value,
+      Contact: userContactNumber.value,
+      Address: userAddress.value,
+      Bio: userBio.value,
+    },
+  ];
+  console.log(usersDataArray[0]);
 });
 // Character limit functionality
 userBio.addEventListener("input", () => {
   if (userBio.value.length > 300) {
     bioLimitExceed.innerHTML = `Characters Limit Exceeded`;
+    userBio.style.outlineColor = "#a31b1b";
+    userBio.style.border = "2px solid #a31b1b";
   } else {
     bioLimitExceed.innerHTML = ``;
+    userBio.style.outlineColor = "#90c5cf";
+    userBio.style.border = "2px solid #90c5cf";
   }
   bioLimitText.innerHTML = `${userBio.value.length}/300`;
 });
