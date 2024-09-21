2625be763c46e7f7392e11effa3507c903def4e2
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-09-21 15:44:10 +0500

    CreateNewElement Function Updated

--- Assets/javascript/createElement.js
+++ Assets/javascript/createElement.js
@@ -1,38 +1,37 @@
-let createNewElement = (
-    elemName,
-    elemClass = null,
-    elemParent,
-    elemContent = null,
-    attributes = {}
-  ) => {
-    let newElement = document.createElement(`${elemName}`);
-    if (Array.isArray(elemClass)) {
-      elemClass.forEach((className) => {
-        newElement.classList.add(className);
-      });
+let createNewElement = ([
+  elemName,
+  elemClass = null,
+  elemParent,
+  elemContent = null,
+  attributes = {},
+]) => {
+  let newElement = document.createElement(`${elemName}`);
+  if (Array.isArray(elemClass)) {
+    elemClass.forEach((className) => {
+      newElement.classList.add(className);
+    });
+  } else if (typeof elemClass === "string") {
+    newElement.classList.add(elemClass);
+  }
+  if (elemContent != null) {
+    newElement.innerHTML = elemContent;
+  }
+  if (attributes && typeof attributes === "object") {
+    for (const attrKey in attributes) {
+      newElement.setAttribute(attrKey, attributes[attrKey]);
+    }
+  }
+  if (typeof elemParent === "string") {
+    let selectedParent = document.querySelector(elemParent);
+    if (selectedParent) {
+      selectedParent.appendChild(newElement);
     } else {
-      newElement.classList.add(elemClass);
+      console.log(
+        `Error While creating element "${newElement}" having class name "${elemClass}"`
+      );
     }
-    if (elemContent != null) {
-      newElement.innerHTML = elemContent;
-    }
-    if (attributes) {
-      for (const attrKey in attributes) {
-        newElement.setAttribute(attrKey, attributes[attrKey]);
-      }
-    }
-    if (typeof elemParent === "string") {
-      let selectedParent = document.querySelector(elemParent);
-      if (selectedParent) {
-        selectedParent.appendChild(newElement);
-      } else {
-        console.log(
-          `Error While creating element "${newElement}" having class name "${elemClass}"`
-        );
-      }
-    } else {
-      ParentName.appendChild(newElement);
-    }
-    return newElement;
-  };
-  
+  } else {
+    ParentName.appendChild(newElement);
+  }
+  return newElement;
+};
