7952a38e904e57194406ddbc4e9f85bbad1546c9
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-09-21 15:26:15 +0500

    CreatedNewElement function

---
+++ Assets/javascript/createElement.js
@@ -0,0 +1,38 @@
+let createNewElement = (
+    elemName,
+    elemClass = null,
+    elemParent,
+    elemContent = null,
+    attributes = {}
+  ) => {
+    let newElement = document.createElement(`${elemName}`);
+    if (Array.isArray(elemClass)) {
+      elemClass.forEach((className) => {
+        newElement.classList.add(className);
+      });
+    } else {
+      newElement.classList.add(elemClass);
+    }
+    if (elemContent != null) {
+      newElement.innerHTML = elemContent;
+    }
+    if (attributes) {
+      for (const attrKey in attributes) {
+        newElement.setAttribute(attrKey, attributes[attrKey]);
+      }
+    }
+    if (typeof elemParent === "string") {
+      let selectedParent = document.querySelector(elemParent);
+      if (selectedParent) {
+        selectedParent.appendChild(newElement);
+      } else {
+        console.log(
+          `Error While creating element "${newElement}" having class name "${elemClass}"`
+        );
+      }
+    } else {
+      ParentName.appendChild(newElement);
+    }
+    return newElement;
+  };
+  
