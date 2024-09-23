18b47db33d06bce1a5a74557a9e647e1d1978a2f
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-09-23 15:06:49 +0500

    Footer Written in JS

--- Assets/javascript/components/header.js
+++ Assets/javascript/components/header.js
@@ -1,14 +1,11 @@
-let header = () => {
-  let headerTag = createNewElement(["header", "header", "body"]);
-  let navbar = createNewElement(["nav", "navbar", headerTag]);
-  let logoContainer = createNewElement(["div", "logo_container", navbar]);
-  createNewElement([
-    "img",
-    null,
-    logoContainer,
-    null,
-    { src: "./Assets/images/sticky-note.png" },
-  ]);
-  createNewElement(["h4", "logo_name", logoContainer, `Profiles`]);
-};
-header();
+let headerTag = createNewElement(["header", "header", "body"]);
+let navbar = createNewElement(["nav", "navbar", headerTag]);
+let logoContainer = createNewElement(["div", "logo_container", navbar]);
+createNewElement([
+  "img",
+  null,
+  logoContainer,
+  null,
+  { src: "./Assets/images/sticky-note.png" },
+]);
+createNewElement(["h4", "logo_name", logoContainer, `Profiles`]);
