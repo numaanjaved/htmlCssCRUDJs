4982c3120b6e5fc8944ecca4d418d1dcf8d42ae0
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-09-27 11:02:04 +0500

    profile picture validation enabled

--- Assets/javascript/components/main/form/formValidation.js
+++ Assets/javascript/components/main/form/formValidation.js
@@ -4,8 +4,12 @@ let successMsg = (errorMsgElem) => {
 	errorMsgElem.style.display = "none";
 };
 let errorMsg = (errorMsgElem, errorMsg_) => {
+	console.log(errorMsg_);
+
 	errorMsgElem.innerHTML = errorMsg_;
 	errorMsgElem.style.display = "block";
+
+
 };
 
 let charLengthCheck = (attr) => {
@@ -23,22 +27,21 @@ let attrVal = (attribute, regex) => {
 		check = false;
 	} else {
 		if (!attribute.value.match(regex)) {
-
 			check = false;
 		} else if (!charLengthCheck(attribute)) {
+			errorMsg(attribute, `Character Limit Increased`)
 			check = false;
-
 		}
 	}
 	return check;
 };
+
 let elemValidationCheck = (attribute, regex, errorMsgElem, _errorMsg) => {
 	let ValidationCheck = true;
 	if (attrVal(attribute, regex)) {
 		ValidationCheck = true;
 		successMsg(errorMsgElem);
 	} else {
-		console.log(attribute.value.length)
 		errorMsg(errorMsgElem, _errorMsg);
 	}
 	return ValidationCheck;
@@ -52,9 +55,9 @@ let Validation = () => {
 	elemValidationCheck(userEmail, /^[a-zA-Z0-9]+(?:[._][a-zA-Z0-9]+)*@[A-Za-z]+\.[A-Za-z]{2,}$/, emailErr, `Please fill in a valid email`);
 	elemValidationCheck(userContactNumber, /^[0-9]{2,}[0-9]{7,}$/, contactErr, `Please fill in a valid contact number`);
 	elemValidationCheck(userAddress, /^[a-zA-Z0-9\s,.'-]{3,}$/, addressErr, `Please fill in a valid address`);
-
 	elemValidationCheck(userBio, /^[a-zA-Z0-9,.!?'\s-]+$/, bioErr, `Please fill in profile description or bio`);
 
+	// , `Your bio exceeds the 300 character limit`
 	// let bioValidation = () => {
 	// 	let bioCheck = true;
 	// 	let bioLimit = true;
@@ -92,21 +95,21 @@ let Validation = () => {
 	// 	}
 	// }
 
-	// let profilePicValidation = () => {
-	//   let imgCheck = true;
-	//   if (
-	//     !imgInput.files.length ||
-	//     imgDisplay.src.includes("default_profile.png")
-	//   ) {
-	//     imgCheck = false;
-	//   }
-	//   if (imgCheck) {
-	//     return true;
-	//   } else {
-	//     return false;
-	//   }
-	// };
-
+	let profilePicValidation = (errorMsgElem, errorMsg_) => {
+		let imgCheck = true;
+		if (
+			!imgInput.files.length ||
+			imgDisplay.src.includes("default_profile.png")
+		) {
+			imgCheck = false;
+			errorMsg(errorMsgElem, errorMsg_);
+		} else {
+			successMsg(errorMsgElem);
+			imgCheck = true;
+		}
+		return imgCheck;
+	};
+	profilePicValidation(image_error_msg, `Please upload an image`)
 	// errorMsg(profilePicValidation(), image_error_msg, `Please upload an image`);
 	let userData = [
 		userFirstName.value,
