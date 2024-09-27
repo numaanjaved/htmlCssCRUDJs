3831874c323121d8a6106261d2f215048a7f59cd
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-09-27 11:48:36 +0500

    Extra comments deleted

--- Assets/javascript/components/main/form/formValidation.js
+++ Assets/javascript/components/main/form/formValidation.js
@@ -75,11 +75,6 @@ let Validation = () => {
 	// 	}
 	// };
 
-	// validationResult(
-	// 	bioValidation,
-	// 	bioErr,
-	// 	`Your bio exceeds the 300 character limit`
-	// );
 
 	// if (bioValidation()) {
 	// 	bioErr.innerHTML = ``;
@@ -137,84 +132,3 @@ charLimitCheck();
 //   Bio: userBio.value,
 //   userPicture: URL.createObjectURL(imgInput.files[0]),
 // };
-
-// `Ahmed`,
-// `Ali`,
-// `ahmed12@gmail.com`,
-// `0333333333`,
-// `Lahore`,
-// `Developer`,
-// `src`,
-
-// let lNameValidation = () => {
-//   let lNameCheck = true;
-//   let lNameMatch = true;
-//   if (userLastName.value == "" || userLastName == null) {
-//     lNameCheck = false;
-//   } else {
-//     if (!userLastName.value.match(/^[a-zA-Z\s]+$/)) {
-//       lNameMatch = false;
-//     }
-//   }
-//   if (lNameCheck && lNameMatch) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
-// let emailValidation = () => {
-//   let emailCheck = true;
-//   let emailMatch = true;
-//   if (userEmail.value == "" || userEmail == null) {
-//     emailCheck = false;
-//   } else {
-//     if (
-//       !userEmail.value.match(
-//         /^[a-zA-Z0-9]+(?:[._][a-zA-Z0-9]+)*@[A-Za-z]+\.[A-Za-z]{2,}$/
-//       )
-//     ) {
-//       emailMatch = false;
-//     }
-//   }
-//   if (emailCheck && emailMatch) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
-// let contactNumValidation = () => {
-//   let contactCheck = true;
-//   let contactMatch = true;
-//   if (userContactNumber.value == "" || userContactNumber == null) {
-//     contactCheck = false;
-//   } else {
-//     if (!userContactNumber.value.match(/^[0-9]{2,}[0-9]{7,}$/)) {
-//       contactMatch = false;
-//     }
-//   }
-//   if (contactCheck && contactMatch) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
-// let addressValidation = () => {
-//   let addressCheck = true;
-//   let addressMatch = true;
-//   if (userAddress.value == "" || userAddress == null) {
-//     addressCheck = false;
-//   } else {
-//     if (!userAddress.value.match(/^[a-zA-Z0-9\s,.'-]{3,}$/)) {
-//       addressMatch = false;
-//     }
-//   }
-//   if (addressCheck && addressMatch) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-// slack test again
