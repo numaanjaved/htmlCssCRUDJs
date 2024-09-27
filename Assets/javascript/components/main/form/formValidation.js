d5e0a03fa4f3786ed605cbbb47c15132d560b5bf
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-09-27 17:25:30 +0500

    Extra code in separate  text file

--- Assets/javascript/components/main/form/formValidation.js
+++ Assets/javascript/components/main/form/formValidation.js
@@ -46,7 +46,7 @@ let profilePicValidation = (errorMsgElem) => {
 };
 let elemValidationCheck = (attribute, regex, len, errorMsgElem) => {
 	let checkVal = true;
-	let maxLen=len;
+	let maxLen = len;
 	if (isNull(attribute)) {
 		successMsg(errorMsgElem);
 	}
@@ -60,23 +60,22 @@ let elemValidationCheck = (attribute, regex, len, errorMsgElem) => {
 		errorMsg(errorMsgElem, `Please fill in a valid data`)
 		checkVal = false;
 	}
-	if (!checkLength(attribute,maxLen)) {
+	if (!checkLength(attribute, maxLen)) {
 		errorMsg(errorMsgElem, `Your bio exceeds the ${maxLen} character limit`);
 		checkVal = false;
 	}
 	return checkVal;
 };
 
-// errorMsgElem, errorMsg_
 let Validation = () => {
 	let validationCheck = true;
 	profilePicValidation(image_error_msg);
-	elemValidationCheck(userFirstName, /^[a-zA-Z\s]+$/, 50, FnameErr );
-	elemValidationCheck(userLastName, /^[a-zA-Z\s]+$/, 50,LnameErr);
-	elemValidationCheck(userEmail, /^[a-zA-Z0-9]+(?:[._][a-zA-Z0-9]+)*@[A-Za-z]+\.[A-Za-z]{2,}$/,100,emailErr);
+	elemValidationCheck(userFirstName, /^[a-zA-Z\s]+$/, 50, FnameErr);
+	elemValidationCheck(userLastName, /^[a-zA-Z\s]+$/, 50, LnameErr);
+	elemValidationCheck(userEmail, /^[a-zA-Z0-9]+(?:[._][a-zA-Z0-9]+)*@[A-Za-z]+\.[A-Za-z]{2,}$/, 100, emailErr);
 	elemValidationCheck(userContactNumber, /^[0-9]{2,}[0-9]{7,}$/, 20, contactErr);
-	elemValidationCheck(userAddress, /^[a-zA-Z0-9\s,.'-]{3,}$/,150,addressErr);
-	elemValidationCheck(userBio, /^[a-zA-Z0-9,.!?'\s-]+$/,300,bioErr);
+	elemValidationCheck(userAddress, /^[a-zA-Z0-9\s,.'-]{3,}$/, 150, addressErr);
+	elemValidationCheck(userBio, /^[a-zA-Z0-9,.!?'\s-]+$/, 300, bioErr);
 
 	let userData = [
 		userFirstName.value,
@@ -93,75 +92,3 @@ let Validation = () => {
 	refreshRecords();
 };
 charLimitCheck();
-
-// console.log();
-// {
-//   FirstName: userFirstName.value,
-//   LastName: userLastName.value,
-//   Email: userEmail.value,
-//   Contact: userContactNumber.value,
-//   Address: userAddress.value,
-//   Bio: userBio.value,
-//   userPicture: URL.createObjectURL(imgInput.files[0]),
-// };
-
-
-
-
-
-
-
-
-	// let result = elemValidationCheck(userFirstName, /^[a-zA-Z\s]+$/);
-	// console.log(result)
-	// elemValidationCheck()
-	// FnameErr, `Please fill in a valid first name`
-	//  LnameErr, `Please fill in a valid last name`
-	// emailErr, `Please fill in a valid email`
-	//  contactErr, `Please fill in a valid contact number`
-	//  addressErr, `Please fill in a valid address`
-	//  bioErr, `Please fill in profile description or bio`
-
-
-	// jab values 2 hain 
-	// errors are diff
-	// enum
-
-
-
-
-	// , `Your bio exceeds the 300 character limit`
-	// let bioValidation = () => {
-	// 	let bioCheck = true;
-	// 	let bioLimit = true;
-	// 	if (userBio.value == "" || userBio == null) {
-	// 		bioCheck = false;
-	// 	} else {
-	// 		if (userBio.value.length > 300) {
-	// 			bioLimit = false;
-	// 		}
-	// 	}
-	// 	if (bioCheck && bioLimit) {
-	// 		return true;
-	// 	} else {
-	// 		return false;
-	// 	}
-	// };
-
-
-	// if (bioValidation()) {
-	// 	bioErr.innerHTML = ``;
-	// 	bioErr.style.display = "none";
-	// } else {
-	// 	if (userBio.value.length > 300) {
-	// 		bioErr.innerHTML = `Your bio exceeds the 300 character limit`;
-	// 		bioErr.style.display = "block";
-	// 		bioLimitExceed.innerHTML = `Characters Limit Exceeded`;
-	// 	} else {
-	// 		bioErr.innerHTML = `Please fill in profile description or bio`;
-	// 		bioErr.style.display = "block";
-	// 	}
-	// }
-
-
-	// errorMsg(profilePicValidation(), image_error_msg, `Please upload an image`);
