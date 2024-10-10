071178499d783616a3b0e6635a86e6538508779b
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-10 14:17:32 +0500

    validation function code moved to class methods

--- Assets/javascript/components/main/form/formValidation.js
+++ Assets/javascript/components/main/form/formValidation.js
@@ -1,4 +1,19 @@
 let userIndexCheck = null;
+let error = [
+	{ errorName: "Null Error", errorMessage: "Please fill data in the field" },
+	{ errorName: "Format Error", errorMessage: "Please match the requested format" },
+	{ errorName: "Limit Exceed", errorMessage: "Characters limit exceeded" },
+	{ errorName: "No picture", errorMessage: "Please Upload an Image" }
+];
+let errorContainer = {
+	user_Fname: FnameErr,
+	user_Lname: LnameErr,
+	user_email: emailErr,
+	user_contact: contactErr,
+	user_address: addressErr,
+	user_bio: bioErr,
+	image: image_error_msg
+};
 class FormValidation {
 	#attribute;
 	#regex;
@@ -10,7 +25,16 @@ class FormValidation {
 	getRegex() { return this.#regex; };
 	setLength(length) { this.#len = length; };
 	getLength() { return this.#len; };
-
+	successMsg(statusMsgElem) {
+		let error = errorContainer[statusMsgElem.name];
+		error.innerHTML = "";
+		error.style.display = "none";
+	};
+	errorMsg(statusMsgElem, errorMsg_) {
+		let error = errorContainer[statusMsgElem.name];
+		error.style.display = "block";
+		error.innerHTML = errorMsg_;
+	};
 	isNull(attribute) {
 		let check = true;
 		if (attribute.value == "" || attribute == null) {
@@ -38,20 +62,32 @@ class FormValidation {
 		this.setRegex(regex);
 		this.setLength(len);
 		if (!this.isNull(this.getAttribute())) {
-			errorMsg(this.getAttribute(), `${error[0].errorName}: ${error[0].errorMessage}`);
+			this.errorMsg(this.getAttribute(), `${error[0].errorName}: ${error[0].errorMessage}`);
 			return false;
-		} else { successMsg(this.getAttribute()); }
+		} else { this.successMsg(this.getAttribute()); }
 		if (!this.matchRegex(this.getAttribute(), this.getRegex())) {
-			errorMsg(this.getAttribute(), `${error[1].errorName}: ${error[1].errorMessage}`);
+			this.errorMsg(this.getAttribute(), `${error[1].errorName}: ${error[1].errorMessage}`);
 			return false;
-		} else { successMsg(this.getAttribute()); }
+		} else { this.successMsg(this.getAttribute()); }
 		if (!this.checkLength(this.getAttribute(), this.getLength())) {
-			errorMsg(this.getAttribute(), `${error[2].errorName}: ${error[2].errorMessage}`);
+			this.errorMsg(this.getAttribute(), `${error[2].errorName}: ${error[2].errorMessage}`);
 			return false;
-		} else { successMsg(this.getAttribute()); }
+		} else { this.successMsg(this.getAttribute()); }
 		return true;
 	};
-
+	profilePicValidation(attribute) {
+		this.setAttribute(attribute);
+		this.getAttribute();
+		let imgCheck = true;
+		if (!this.getAttribute().files.length || imgDisplay.src.includes("default_profile.png")) {
+			imgCheck = false;
+			this.errorMsg(attribute, `${error[3].errorName}: ${error[3].errorMessage}`)
+		} else {
+			this.successMsg(attribute);
+			imgCheck = true;
+		}
+		return imgCheck;
+	};
 	createUser(userDataArr) {
 		let userDataObj = new User();
 		userDataObj.create(userDataArr);
@@ -66,50 +102,13 @@ class FormValidation {
 		}
 	};
 
-};
-let error = [
-	{ errorName: "Null Error", errorMessage: "Please fill data in the field" },
-	{ errorName: "Format Error", errorMessage: "Please match the requested format" },
-	{ errorName: "Limit Exceed", errorMessage: "Characters limit exceeded" },
-	{ errorName: "No picture", errorMessage: "Please Upload an Image" }
-];
-let errorContainer = {
-	user_Fname: FnameErr,
-	user_Lname: LnameErr,
-	user_email: emailErr,
-	user_contact: contactErr,
-	user_address: addressErr,
-	user_bio: bioErr,
-	image: image_error_msg
-};
-let successMsg = (statusMsgElem) => {
-	let error = errorContainer[statusMsgElem.name];
-	error.innerHTML = "";
-	error.style.display = "none";
-};
-let errorMsg = (statusMsgElem, errorMsg_) => {
-	let error = errorContainer[statusMsgElem.name];
-	error.style.display = "block";
-	error.innerHTML = errorMsg_;
-};
 
-let profilePicValidation = (attribute) => {
-	let imgCheck = true;
-	if (!attribute.files.length || imgDisplay.src.includes("default_profile.png")) {
-		imgCheck = false;
-		errorMsg(attribute, `${error[3].errorName}: ${error[3].errorMessage}`)
-	} else {
-		successMsg(attribute);
-		imgCheck = true;
-	}
-	return imgCheck;
 };
-
 
 let Validation = () => {
 	let validationCheck = true;
 	let newForm = new FormValidation();
-	if (!profilePicValidation(imgInput)) { validationCheck = false; }
+	if (!newForm.profilePicValidation(imgInput)) { validationCheck = false; }
 	if (!newForm.elemValidationCheck(userFirstName, /^[a-zA-Z\s]*$/, 30)) { validationCheck = false; }
 	if (!newForm.elemValidationCheck(userLastName, /^[a-zA-Z\s]*$/, 30)) { validationCheck = false; }
 	if (!newForm.elemValidationCheck(userEmail, /^[a-zA-Z0-9]+(?:[._][a-zA-Z0-9]+)*@[A-Za-z]+\.[A-Za-z]{2,}$/, 100)) { validationCheck = false; }
