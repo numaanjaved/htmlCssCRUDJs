211d3af4155c94a9901b6604e7b8d3527ec56241
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-14 11:12:03 +0500

    update code refactored

--- Assets/javascript/components/main/recordButtons.js
+++ Assets/javascript/components/main/recordButtons.js
@@ -1,3 +1,29 @@
+let feedUpdateForm = (selectedRecord) => {
+	userFirstName.value = usersDataArray[selectedRecord].getFirstName();
+	userLastName.value = usersDataArray[selectedRecord].getLastName();
+	userEmail.value = usersDataArray[selectedRecord].getEmail();
+	userContactNumber.value = usersDataArray[selectedRecord].getContactNum();
+	userAddress.value = usersDataArray[selectedRecord].getAddress();
+	userBio.value = usersDataArray[selectedRecord].getBio();
+	imgDisplay.src = usersDataArray[selectedRecord].getProfilePic();
+	updateAdmin(selectedRecord);
+}
+let updateAdmin = (admin) => {
+	if (usersDataArray[admin].getUserType() === "Admin") {
+		let adminOption = document.getElementById(`admin_option`);
+		adminOption.setAttribute("selected", "selected");
+		adminAttContainer.style.display = "flex";
+		admin_heading.style.display = "block";
+		userName.value = usersDataArray[admin].getAdminName();
+		userPassword.value = usersDataArray[admin].getPassword();
+	}
+}
+let hideSelect = () => {
+	let select = document.getElementById(`select_user`);
+	let selectUserHeading = document.getElementById(`choose_user_heading`);
+	select.style.display = "none";
+	selectUserHeading.style.display = "none";
+}
 let readProfile = (readProfileBtn) => {
 	let clickedBtnId = readProfileBtn.parentElement.parentElement.nextSibling.id;
 	let modal = usersDataArray.find((user) => user.getID() === clickedBtnId);
@@ -11,35 +37,12 @@ let delProfile = (delProfileBtn) => {
 	if (user) { user.delete(); }
 };
 let updateProfile = (updateProfileBtn) => {
+	hideSelect();
 	window.scrollTo(200, 0);
 	let clickedBtnId = updateProfileBtn.parentElement.parentElement.nextSibling.id;
 	let selectedUser = usersDataArray.findIndex((user) => user.getID() === clickedBtnId);
 	userIndexCheck = selectedUser;
-	userFirstName.value = usersDataArray[selectedUser].getFirstName();
-	userLastName.value = usersDataArray[selectedUser].getLastName();
-	userEmail.value = usersDataArray[selectedUser].getEmail();
-	userContactNumber.value = usersDataArray[selectedUser].getContactNum();
-	userAddress.value = usersDataArray[selectedUser].getAddress();
-	userBio.value = usersDataArray[selectedUser].getBio();
-	imgDisplay.src = usersDataArray[selectedUser].getProfilePic();
-	if (usersDataArray[selectedUser].getUserType() === "Admin") {
-		let select = document.getElementById(`select_user`);
-		let selectUserHeading = document.getElementById(`choose_user_heading`);
-		select.style.display = "none";
-		selectUserHeading.style.display = "none";
-		let adminOption = document.getElementById(`admin_option`);
-		adminOption.setAttribute("selected", "selected");
-		adminAttContainer.style.display = "flex";
-		admin_heading.style.display = "block";
-		userName.value = usersDataArray[selectedUser].getAdminName();
-		userPassword.value = usersDataArray[selectedUser].getPassword();
-	}
-	if (usersDataArray[selectedUser].getUserType() === "User") {
-		let select = document.getElementById(`select_user`);
-		let selectUserHeading = document.getElementById(`choose_user_heading`);
-		select.style.display = "none";
-		selectUserHeading.style.display = "none";
-	}
+	feedUpdateForm(selectedUser);
 };
 
 let readUpdateDelete = (userDataContainer, ProfileBtnOpsContainer, id) => {
