ed4fba3a511211ba206edee9556401a85fff5ea9
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-14 15:18:19 +0500

    All the files related to views moved into views folder

--- Assets/javascript/components/main/form/formValidation.js
+++ Assets/javascript/components/main/form/formValidation.js
@@ -1,14 +1,4 @@
 let userIndexCheck = null;
-let fromReset = () => {
-	form.reset();
-	adminAttContainer.style.display = "none";
-	admin_heading.style.display = "none";
-	imgDisplay.src = "./Assets/images/default_profile.png";
-	let select = document.getElementById(`select_user`);
-	let selectUserHeading = document.getElementById(`choose_user_heading`);
-	select.style.display = "flex";
-	selectUserHeading.style.display = "block";
-};
 let formValidation = () => {
 	let validationCheck = true;
 	let userInstance = new User();
